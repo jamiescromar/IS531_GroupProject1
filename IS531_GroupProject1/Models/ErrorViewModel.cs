cfeb3e82b3a561c271cfcd554cc801902d0badb5
jamiescromar <jamiescromar@gmail.com>
2021-10-04 12:25:59 -0600

    Add project files.

---
+++ IS531_GroupProject1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IS531_GroupProject1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
